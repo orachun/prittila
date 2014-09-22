cfcc1bfb9cb8e6a14edc2398fb9cd4667fc1db29
orachun <orachun_chun@hotmail.com>
2014-09-22 22:45:09 +0700

    add comming soon page

---
+++ application/views/comming.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            body
+            {
+                background-color: #c5a5b0;
+                
+            }
+            .logo
+            {
+                font-family: 'Fredericka the Great', cursive;
+                font-size: 2.5em;
+                color: white;
+            }
+            #content
+            {
+                width: 300px;
+                margin: auto;
+            }
+            </style>
+    </head>
+    <body>
+        <div id="content">
+            <h1 class="logo">Prittila</h1>
+            <h2 class="logo">Comming Soon</h2>
+            <div>พบกับเว็บ Prittila เร็วๆนี้</div>
+        </div>
+    </body>
+</html>
